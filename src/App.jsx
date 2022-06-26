dddb344aa70067ee7461bec36fe694b258a63aa0
Mirzokhid Nazirov <mirzokhidnazirov97@gmail.com>
2022-06-26 18:27:23 +0500

    redirect when trying to access /world; redirect for incorrect path in url

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
 import MapPage from './components/Map';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home';
+import { useSelector } from "react-redux";
 
 function App() {
+  const count = Number(useSelector((state) => state.counter.value));
+
   return (
     <AppDiv>
       <Routes>
         <Route element={<Home />} path='/' />
-        <Route element={<MapPage neededLoaded={3} />} path='/world'  />
+        <Route element={count !== 0 ? <MapPage neededLoaded={3} /> : <Navigate to="/" replace />} path='/world'  />
+        <Route element={<Navigate to='/' replace />} path="*" />
       </Routes>
     </AppDiv>
   );
